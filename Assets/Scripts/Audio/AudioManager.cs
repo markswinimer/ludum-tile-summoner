8cfb35a111ea73e0807d4d057ca41d0a65ad4228
Mark Swinimer <markswinimer@gmail.com>
2024-04-14 10:47:42 -0400

    adds scross scene audio

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -6,8 +7,26 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-        // plays the audio when scene loads
+        DontDestroyOnLoad(gameObject);  // prevents the audio from stopping between scenes
         audioSource = GetComponent<AudioSource>();
         audioSource.Play();
+    }
+    public void FadeOutAndStop(float fadeTime)
+    {
+        StartCoroutine(FadeOutCoroutine(fadeTime));
+    }
+
+    private IEnumerator FadeOutCoroutine(float fadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+            yield return null;
+        }
+
+        audioSource.Stop();
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -1,17 +1,54 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
 
 public class StartMenu : MonoBehaviour
 {
-    // button attached via TMP button UI
+    // elements on start menu scene
+    public AudioManager audioManager;
+    public CanvasGroup uiElement;
+
+    void Start()
+    {
+        // UI starts slightly faded. to match the music
+        uiElement.alpha = 0.3f;
+
+        // Start fade in
+        StartCoroutine(FadeInUI());
+    }
+    IEnumerator FadeInUI()
+    {
+        float fadeDuration = 1f;
+        while (uiElement.alpha < 1)
+        {
+            uiElement.alpha += Time.deltaTime / fadeDuration;
+            yield return null;
+        }
+    }
+
     public void StartGame()
     {
-        Debug.Log("Start Game");
-        SceneManager.LoadScene("MainScene");
+        StartCoroutine(FadeAndLoadScene());
+    }
+
+    IEnumerator FadeAndLoadScene()
+    {
+        // Fade out the UI
+        float fadeDuration = 1f;
+        while (uiElement.alpha > 0)
+        {
+            uiElement.alpha -= Time.deltaTime / fadeDuration;
+            yield return null;
+        }
+
+        // Load the next scene
+        SceneManager.LoadScene("MARK");
     }
     public void ExitGame()
     {
-        Debug.Log("Exit game");
+        //may need to alter this for different builds? aka web5
         Application.Quit();
     }
 }
