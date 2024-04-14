6d1782c4c5a92c86ca2fbfe6f99b166798558091
Ryaneklipse <rchild0916@gmail.com>
2024-04-14 11:55:15 -0400

    Add player death, respawn

--- Assets/Scripts/KillZoneDetector.cs
+++ Assets/Scripts/KillZoneDetector.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDetector : MonoBehaviour
+public class KillZoneDetector : MonoBehaviour
 {
 
     public Player player;
-    private Tile parentTile;
     // Start is called before the first frame update
     void Start()
     {
         player = FindFirstObjectByType<Player>();
-        parentTile = GetComponentInParent<Tile>();
     }
 
     // Update is called once per frame
@@ -22,9 +20,8 @@ public class PlayerDetector : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.GetComponentInParent<Player>() != null){
-            player.currentTile = parentTile;
-            Debug.Log("Changed player tile to " + parentTile.tilePosition.x + "," + parentTile.tilePosition.y);
-            if(parentTile.tileType == TileType.Shrine) player.checkpoint = parentTile;
+            Debug.Log("Player fell and is dead");
+            player.isDead = true;
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     public bool isPlayerControllable { get; private set; }
     public Tile currentTile;
+    public Tile checkpoint;
+    public bool isDead;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(isDead){
+            //respawn at last shrine
+            transform.position = checkpoint.GetComponentInChildren<SpawnPoint>().transform.position;
+            isDead = false;
+        }
+        if(checkpoint == null) checkpoint = currentTile;
     }
 
     public void ChangePlayerControl(){
